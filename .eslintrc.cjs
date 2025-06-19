b8a67cb35d1a0f0c5dadfa857070566be6f134a0
Uday Bhardwaj <udaybh07@gmail.com>
2025-06-19 20:51:10 +0530

    Solving bug-0.4

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,7 +16,8 @@ module.exports = {
     'ecmaVersion': 2018,
     'sourceType': 'module'
   },
-  'plugins': [
+  settings: { react: { version: '18.2' } },
+  plugins: [
     'react', 'jest'
   ],
   'rules': {
