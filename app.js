6a24ba09c1d673f38439ad77df10f0f85514cb06
Uday Bhardwaj <udaybh07@gmail.com>
2025-06-22 19:43:10 +0530

    Deploying v0.4

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get('/version', (req, res) => {
   res.send('1')
 })
 
+app.get('/healthTraveller', (req, res) => {
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
 // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
